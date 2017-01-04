7805dc69450bb05d2943ccc696383f8f720e680c
cikai <cikai0923@gmail.com>
2017-01-04 23:01:43 +0800

    compress json & en(de)code uri

--- src/main.js
+++ src/main.js
@@ -16,19 +16,44 @@ new Vue({
   components: { AppFooter }
 })
 
-/* init app */
+/* json formatter */
 var app = new Vue({
-  el: '#app',
+  el: '#json',
   data: {
-    message: 'Json格式化',
+    json_message: 'Json格式化',
     code: '',
     result: ''
   },
   methods: {
-  	jsonParser: function() {
+  	jsonFormatter: function() {
       let code = JSON.parse(this.code);
   		// 缺少Json格式数据校验 2017/1/2
   		this.result = JSON.stringify(code, null, 4);
-  	}
+  	},
+    jsonCompress: function() {
+      let result = JSON.parse(this.result);
+      // 缺少Json格式数据校验 2017/1/4
+      this.code = JSON.stringify(result);
+    }
+  }
+})
+
+/* decodeuri */
+var app = new Vue({
+  el: '#uri',
+  data: {
+    uri_message: 'URL解码',
+    code: '',
+    result: ''
+  },
+  methods: {
+    decodeUri: function() {
+      let code = this.code;
+      this.result = decodeURI(code);
+    },
+    encodeUri: function() {
+      let result = this.result;
+      this.code = encodeURI(result);
+    }
   }
 })
